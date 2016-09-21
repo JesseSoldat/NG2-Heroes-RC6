ec948ff108bae536edcfe99bbf0a18df7a21c595
jesse soldat <jsoldat@hotmail.com>
2016-09-21 15:21:35 +0700

    can select a hero from the list

--- app/app.component.js
+++ app/app.component.js
@@ -9,13 +9,44 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: 'Admiral Ackbar' },
+    { id: 12, name: 'Greedo' },
+    { id: 13, name: 'C-3PO' },
+    { id: 14, name: 'Qui-Gon Jinn' },
+    { id: 15, name: 'Padmé Amidala' },
+    { id: 16, name: 'Darth Vader' },
+    { id: 17, name: 'Lando Calrissian' },
+    { id: 18, name: 'Darth Maul' },
+    { id: 19, name: 'Emperor Palpatine' },
+    { id: 20, name: 'Obi-Wan Kenobi' },
+    { id: 21, name: 'Yoda' },
+    { id: 22, name: 'R2-D2' },
+    { id: 23, name: 'Boba Fett' },
+    { id: 24, name: 'Chewbacca' },
+    { id: 25, name: ' Princess Leia' },
+    { id: 26, name: 'Luke Skywalker' },
+    { id: 26, name: 'Han Solo' }
+];
 var AppComponent = (function () {
     function AppComponent() {
+        this.heroes = HEROES;
+        this.title = 'Tour of Heroes';
     }
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>JLab</h1>',
+            template: "\n\t<h1>{{title}}</h1>\n\t<div *ngIf=\"selectedHero\">\n\t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n\t\t<div><label>name: </label>\n\t\t<input [(ngModel)]=\"selectedHero.name\"  placeholder=\"name\">\n\t</div>\n\n\t</div>\n\t\n\n\t<h2>My Heroes</h2>\n\t<ul class=\"heroes\">\n\t\t<li *ngFor=\"let hero of heroes\" (click)=\"onSelect(hero)\">\n\t\t<span class=\"badge\">{{hero.id}}</span>{{hero.name}}\n\t\t</li>\n\t</ul>\n\t",
+            styles: ["\n\t  .selected {\n\t    background-color: #CFD8DC !important;\n\t    color: white;\n\t  }\n\t  .heroes {\n\t    margin: 0 0 2em 0;\n\t    list-style-type: none;\n\t    padding: 0;\n\t    width: 15em;\n\t  }\n\t  .heroes li {\n\t    cursor: pointer;\n\t    position: relative;\n\t    left: 0;\n\t    background-color: #EEE;\n\t    margin: .5em;\n\t    padding: .3em 0;\n\t    height: 1.6em;\n\t    border-radius: 4px;\n\t  }\n\t  .heroes li.selected:hover {\n\t    background-color: #BBD8DC !important;\n\t    color: white;\n\t  }\n\t  .heroes li:hover {\n\t    color: #607D8B;\n\t    background-color: #DDD;\n\t    left: .1em;\n\t  }\n\t  .heroes .text {\n\t    position: relative;\n\t    top: -3px;\n\t  }\n\t  .heroes .badge {\n\t    display: inline-block;\n\t    font-size: small;\n\t    color: white;\n\t    padding: 0.8em 0.7em 0 0.7em;\n\t    background-color: #607D8B;\n\t    line-height: 1em;\n\t    position: relative;\n\t    left: -1px;\n\t    top: -4px;\n\t    height: 1.8em;\n\t    margin-right: .8em;\n\t    border-radius: 4px 0 0 4px;\n\t  }\n\t"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,116 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Admiral Ackbar'},
+  { id: 12, name: 'Greedo' },
+  { id: 13, name: 'C-3PO' },
+  { id: 14, name: 'Qui-Gon Jinn' },
+  { id: 15, name: 'Padmé Amidala' },
+  { id: 16, name: 'Darth Vader' },
+  { id: 17, name: 'Lando Calrissian' },
+  { id: 18, name: 'Darth Maul' },
+  { id: 19, name: 'Emperor Palpatine' },
+  { id: 20, name: 'Obi-Wan Kenobi' },
+  { id: 21, name: 'Yoda' },
+  { id: 22, name: 'R2-D2' },
+  { id: 23, name: 'Boba Fett' },
+  { id: 24, name: 'Chewbacca' },
+  { id: 25, name: ' Princess Leia' },
+  { id: 26, name: 'Luke Skywalker' },
+  { id: 26, name: 'Han Solo' }
+
+]
+
 @Component({
 	selector: 'my-app',
-	template: '<h1>JLab</h1>',
+	template: `
+	<h1>{{title}}</h1>
+	<div *ngIf="selectedHero">
+		<h2>{{selectedHero.name}} details!</h2>
+		<div><label>id: </label>{{selectedHero.id}}</div>
+		<div><label>name: </label>
+		<input [(ngModel)]="selectedHero.name"  placeholder="name">
+	</div>
+
+	</div>
+	
+
+	<h2>My Heroes</h2>
+	<ul class="heroes">
+		<li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+		<span class="badge">{{hero.id}}</span>{{hero.name}}
+		</li>
+	</ul>
+	`,
+	styles: [`
+	  .selected {
+	    background-color: #CFD8DC !important;
+	    color: white;
+	  }
+	  .heroes {
+	    margin: 0 0 2em 0;
+	    list-style-type: none;
+	    padding: 0;
+	    width: 15em;
+	  }
+	  .heroes li {
+	    cursor: pointer;
+	    position: relative;
+	    left: 0;
+	    background-color: #EEE;
+	    margin: .5em;
+	    padding: .3em 0;
+	    height: 1.6em;
+	    border-radius: 4px;
+	  }
+	  .heroes li.selected:hover {
+	    background-color: #BBD8DC !important;
+	    color: white;
+	  }
+	  .heroes li:hover {
+	    color: #607D8B;
+	    background-color: #DDD;
+	    left: .1em;
+	  }
+	  .heroes .text {
+	    position: relative;
+	    top: -3px;
+	  }
+	  .heroes .badge {
+	    display: inline-block;
+	    font-size: small;
+	    color: white;
+	    padding: 0.8em 0.7em 0 0.7em;
+	    background-color: #607D8B;
+	    line-height: 1em;
+	    position: relative;
+	    left: -1px;
+	    top: -4px;
+	    height: 1.8em;
+	    margin-right: .8em;
+	    border-radius: 4px 0 0 4px;
+	  }
+	`]
+
 
 })
 
-export class AppComponent { }
+export class AppComponent { 
+
+	heroes = HEROES;
+
+	selectedHero: Hero;
+
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+
+	title = 'Tour of Heroes';
+	
+
+}
